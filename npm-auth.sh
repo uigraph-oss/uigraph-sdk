e74f655eee1c4e45b1999cdc790ed863ff207101
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-02-04 02:43:42 +0600

    chore: remove deprecated external connection command from npm-auth.sh

--- npm-auth.sh
+++ npm-auth.sh
@@ -1,8 +1,3 @@
-aws codeartifact associate-external-connection \
-	--domain uigraph-sdk \
-	--repository npm \
-	--external-connection public:npmjs
-
 aws codeartifact login \
 	--tool npm \
 	--domain uigraph-sdk \
