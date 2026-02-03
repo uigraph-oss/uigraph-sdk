a8dc69b8e6e66b2935feaa92e592869c9183edc7
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-02-04 03:59:21 +0600

    chore: enhance npm-auth.sh with error handling and improved AWS CodeArtifact token retrieval

--- npm-auth.sh
+++ npm-auth.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+set -e
+
 AWS_REGION="us-east-1"
-AWS_REPO="npm-sdk"
-AWS_DOMAIN="uigraph"
 AWS_DOMAIN_OWNER="120516861714"
+AWS_DOMAIN="uigraph"
+AWS_REPO="npm-sdk"
+
+echo "> Npm base registry: $(npm config get registry)"
+echo "> @uigraph registry: $(npm config get @uigraph:registry)"
 
 TOKEN=$(aws codeartifact get-authorization-token \
   --region $AWS_REGION \
