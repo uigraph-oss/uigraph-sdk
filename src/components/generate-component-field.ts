a7e8ca2131109d32b2e7355de434f62f692a0491
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-02-03 20:33:51 +0600

    chore: update version to 0.0.13 in package.json and refine type definitions in ServerComponentFieldInput for better clarity

---
+++ src/components/generate-component-field.ts
@@ -0,0 +1,21 @@
+import { ServerComponentField } from '@/types'
+import { buildMetaData } from './data-structure'
+
+type GenerateComponentFieldOptions = Partial<ServerComponentField>
+
+export function generateComponentFieldInput(
+  options: GenerateComponentFieldOptions
+) {
+  const componentId = options.componentFieldId ?? String(Math.random())
+
+  const field = {
+    ...options,
+    componentFieldId: componentId,
+  }
+
+  const [metaData] = buildMetaData([field], {
+    [componentId]: options.data,
+  })
+
+  return metaData
+}
