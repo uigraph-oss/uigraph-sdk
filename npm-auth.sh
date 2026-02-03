4f76dd37393566e90a0f3610585e525b3fea7960
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-02-04 03:41:37 +0600

    chore: remove .envrc file and update npm-auth.sh for improved AWS CodeArtifact authentication

--- npm-auth.sh
+++ npm-auth.sh
@@ -1,4 +1,15 @@
-aws codeartifact login \
-	--tool npm \
-	--domain uigraph-sdk \
-	--repository npm
+#!/bin/sh
+
+AWS_REGION="us-east-1"
+AWS_REPO="npm-sdk"
+AWS_DOMAIN="uigraph"
+AWS_DOMAIN_OWNER="120516861714"
+
+TOKEN=$(aws codeartifact get-authorization-token \
+  --region $AWS_REGION \
+  --domain $AWS_DOMAIN \
+  --domain-owner $AWS_DOMAIN_OWNER \
+  --query authorizationToken --output text)
+
+npm config set @$AWS_DOMAIN:registry=https://$AWS_DOMAIN-$AWS_DOMAIN_OWNER.d.codeartifact.$AWS_REGION.amazonaws.com/npm/$AWS_REPO/
+npm config set //${AWS_DOMAIN}-${AWS_DOMAIN_OWNER}.d.codeartifact.${AWS_REGION}.amazonaws.com/npm/$AWS_REPO/:_authToken=$TOKEN
