d57988136f872a0e85f7ab6c33afb5121b48df5a
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-02-04 04:01:36 +0600

    chore: update npm-auth.sh for consistent output formatting and add registry setting message

--- npm-auth.sh
+++ npm-auth.sh
@@ -7,14 +7,15 @@ AWS_DOMAIN_OWNER="120516861714"
 AWS_DOMAIN="uigraph"
 AWS_REPO="npm-sdk"
 
-echo "> Npm base registry: $(npm config get registry)"
+echo "> npm base registry: $(npm config get registry)"
 echo "> @uigraph registry: $(npm config get @uigraph:registry)"
 
 TOKEN=$(aws codeartifact get-authorization-token \
-  --region $AWS_REGION \
-  --domain $AWS_DOMAIN \
-  --domain-owner $AWS_DOMAIN_OWNER \
-  --query authorizationToken --output text)
+	--region $AWS_REGION \
+	--domain $AWS_DOMAIN \
+	--domain-owner $AWS_DOMAIN_OWNER \
+	--query authorizationToken --output text)
 
+echo "> Setting npm @$AWS_DOMAIN:registry"
 npm config set @$AWS_DOMAIN:registry=https://$AWS_DOMAIN-$AWS_DOMAIN_OWNER.d.codeartifact.$AWS_REGION.amazonaws.com/npm/$AWS_REPO/
 npm config set //${AWS_DOMAIN}-${AWS_DOMAIN_OWNER}.d.codeartifact.${AWS_REGION}.amazonaws.com/npm/$AWS_REPO/:_authToken=$TOKEN
