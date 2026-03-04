f430bbe676aa1407513b97230c98ce2be605f093
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-03-04 19:37:25 +0600

    feat: integrate NoSQL parsing into SQL parser for enhanced compatibility

--- src/nosql-parser/index.ts
+++ src/nosql-parser/index.ts
@@ -1,2 +1,32 @@
+import {
+  convertDynamoSchemaToAst,
+  convertJsonSchemaToAst,
+  convertMongoSchemaToAst,
+} from './convert-ast'
+import {
+  DynamoEditorSchema,
+  JsonEditorSchema,
+  MongoEditorSchema,
+} from './nosql-schema'
+
 export * from './convert-ast'
 export * from './nosql-schema'
+
+export function convertNoSQLToAst(input: unknown) {
+  const dynamoDbSchema = DynamoEditorSchema.safeParse(input)
+  if (dynamoDbSchema.success) {
+    return convertDynamoSchemaToAst(dynamoDbSchema.data)
+  }
+
+  const mongoDbSchema = MongoEditorSchema.safeParse(input)
+  if (mongoDbSchema.success) {
+    return convertMongoSchemaToAst(mongoDbSchema.data)
+  }
+
+  const jsonSchema = JsonEditorSchema.safeParse(input)
+  if (jsonSchema.success) {
+    return convertJsonSchemaToAst(jsonSchema.data)
+  }
+
+  throw new Error('Input does not match any supported NoSQL schema format')
+}
