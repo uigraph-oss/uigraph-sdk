0fe1c13986cacd75d22a34d2851d9acdaeb1cc77
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-06-23 10:31:11 +0600

    Refactor code structure for improved readability and maintainability

--- src/__lab__/index.ts
+++
@@ -1,8 +0,0 @@
-import stripePaymentJson from '../../demo/stripe-payment-uig.json'
-import { convertUiGraphToMermaid, UiGraphInput } from '../uig-converter'
-
-console.log(
-  convertUiGraphToMermaid(stripePaymentJson as UiGraphInput, {
-    detailedContext: true,
-  }).mermaid
-)
